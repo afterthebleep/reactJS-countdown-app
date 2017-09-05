de39112f84f889ba44dc4946883a8b3db7446133
aishao <afterthebleep@gmail.com>
2017-09-04 22:33:46 -0700

    added newDeadline feature to input and setState for appropriate components | Minor refactor of class names for clarity

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
+import Counter from './counter'
 import './app.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
       this.state = {
-        deadline: 'March 27, 2018'
+        deadline: 'March 27, 2018',
+        newDeadline: ''
       }
+  }
+
+  changeDeadline() {
+    this.setState({deadline: this.state.newDeadline})
   }
 
   render() {
@@ -15,15 +21,18 @@ class App extends Component {
         <div className='app-title'>
           Countdown to {this.state.deadline}
         </div>
+        <Counter />
         <div>
-          <div className='days'>14 days</div>
-          <div className='hours'>30 hours</div>
-          <div className='mins'>15 minutes</div>
-          <div className='secs'>20 seconds</div>
-        </div>
-        <div>
-          <input className='input' placeholder='new date'/>
-          <button type="button">Submit</button>
+          <input
+            className='input'
+            onChange={event => this.setState({newDeadline: event.target.value})}
+            placeholder='new date'
+          />
+          <button
+            type="button"
+            onClick={() => this.changeDeadline()}>
+            Submit
+          </button>
         </div>
       </div>
     )
